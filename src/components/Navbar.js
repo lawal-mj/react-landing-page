864b3bb1e150949102d4fd7806ecf237a6f7cad4
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-05 18:51:41 +0100

    add background to home button and change color of  background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
-    <div className="flex justify-between px-2 py-2 bg-slate-500 text-lg">
+    <div className="flex justify-between px-4 py-3 bg-blue-500 text-lg font-bold text-white">
       {/* the left sided items */}
-      <div className="flex gap-7">
-        <p>Home</p>
+      <div className="flex gap-8">
+        <p className="bg-red-500 px-2 rounded-md">Home</p>
         <p>About</p>
         <p>Contact</p>
       </div>

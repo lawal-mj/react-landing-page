4457d042a413d98acd58dbd5d379bc87b2115d28
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-06-05 18:37:30 +0100

    add items to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,17 @@
-export default function Navbar(){
-    return (
-        <h1>This is a navbar</h1>
-    )
+export default function Navbar() {
+  return (
+    <div>
+      {/* the left sided items */}
+      <div>
+        <p>Home</p>
+        <p>About</p>
+        <p>Contact</p>
+      </div>
+
+      {/* the right sided items  */}
+      <div>
+        <p>Menu</p>
+      </div>
+    </div>
+  );
 }
